d76e32d71d0a417bb4dff1be4b68beeb31d2d756
Agustin Carnevale <agustinc.dev@gmail.com>
2025-04-01 09:55:53 -0300

    read cli arguments

--- main.go
+++ main.go
@@ -1,25 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// args without the program name
+	args := os.Args[1:]
+	if len(args) < 1 {
+		// Println + exit code 1
+		log.Fatalln("no website provided")
+	}
+	if len(args) > 1 {
+		// Println + exit code 1
+		log.Fatalln("too many arguments provided")
+	}
 
-	// fmt.Println(normalizeURL("https://blog.boot.dev/path"))
+	baseUrl := args[0]
 
-	inputURL := "https://blog.boot.dev"
-	inputBody := `
-<html>
-<body>
-	<a href="/path/one">
-		<span>Boot.dev</span>
-	</a>
-	<a href="https://other.com/path/one">
-		<span>Boot.dev</span>
-	</a>
-</body>
-</html>
-`
+	fmt.Println("starting crawl of:", baseUrl)
 
-	getURLsFromHTML(inputBody, inputURL)
+	// 	// fmt.Println(normalizeURL("https://blog.boot.dev/path"))
+
+	// 	inputURL := "https://blog.boot.dev"
+	// 	inputBody := `
+	// <html>
+	// <body>
+	// 	<a href="/path/one">
+	// 		<span>Boot.dev</span>
+	// 	</a>
+	// 	<a href="https://other.com/path/one">
+	// 		<span>Boot.dev</span>
+	// 	</a>
+	// </body>
+	// </html>
+	// `
+
+	// getURLsFromHTML(inputBody, inputURL)
 }
